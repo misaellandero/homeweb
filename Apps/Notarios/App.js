1288fc87bf971c038ba31967a388484f9e91c93f
Misael Landero <francisco.landero@dexcom.com>
2022-12-07 08:46:52 -0800

    Fix key

--- Apps/Notarios/App.js
+++ Apps/Notarios/App.js
@@ -1,3 +1,16 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBUYrR23eSc2tyZDJQROBo3ZRd3Qd7dblE",
+    authDomain: "notarios-2eff8.firebaseapp.com",
+    databaseURL: "https://notarios-2eff8.firebaseio.com",
+    projectId: "notarios-2eff8",
+    storageBucket: "notarios-2eff8.appspot.com",
+    messagingSenderId: "949546437729",
+    appId: "1:949546437729:web:f29d2c202a859677082e20"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
     .then((reg)=> console.log('Service worked registered', reg))
